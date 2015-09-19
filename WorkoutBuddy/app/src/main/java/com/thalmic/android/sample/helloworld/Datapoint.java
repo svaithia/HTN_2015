a5288af557a845c9ad5835aed2f9f9b28c48b810
Abhijit Kambalapally <abhijitkamb@gmail.com>
2015-09-19 18:36:32 -0400

    algo impl

---
+++ WorkoutBuddy/app/src/main/java/com/thalmic/android/sample/helloworld/Datapoint.java
@@ -0,0 +1,41 @@
+package com.thalmic.android.sample.helloworld;
+
+/**
+ * Created by Abhijit on 9/19/2015.
+ */
+public class Datapoint {
+    private float _roll, _pitch, _yaw;
+    private long _timestamp;
+
+    public Datapoint(long timestamp, float roll, float pitch, float yaw){
+        _roll = roll;
+        _pitch = pitch;
+        _yaw = yaw;
+        _timestamp = timestamp;
+    }
+
+    public float getRoll(){
+        return _roll;
+    }
+
+    public float getPitch(){
+        return _pitch;
+    }
+
+    public float getYaw(){
+        return _yaw;
+    }
+
+    public float getTimestamp(){
+        return _timestamp;
+    }
+
+
+
+    public void set(long timestamp, float roll, float pitch, float yaw){
+        _timestamp = timestamp;
+        _roll = roll;
+        _pitch = pitch;
+        _yaw = yaw;
+    }
+}
--- WorkoutBuddy/app/src/main/java/com/thalmic/android/sample/helloworld/HelloWorldActivity.java
+++ WorkoutBuddy/app/src/main/java/com/thalmic/android/sample/helloworld/HelloWorldActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,10 +26,33 @@ import com.thalmic.myo.Quaternion;
 import com.thalmic.myo.XDirection;
 import com.thalmic.myo.scanner.ScanActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorldActivity extends Activity {
 
     private TextView mLockStateView;
     private TextView mTextView;
+    List<Datapoint> data_list = new ArrayList<Datapoint>();
+    List<Float> peak_pitch = new ArrayList<Float>();
+    List<Float> dip_pitch = new ArrayList<Float>();
+
+    long ts_interval = 100;
+    int cal_num_reps = 0;
+    long ts_begin = 0;
+    long ts_prev = 0;
+
+    float p_curr = 0;
+    float p_prev = 0;
+    float p_prevprev = 0;
+
+    long ts_count = 0;
+    boolean listdata = true;
+
+    final static double[] dlist = { 18.524588, 19.580612, 16.52172, 11.724332, 3.2621462, -9.4709215, -26.468754, -41.036163, -50.80451, -50.98596, -42.90037, -26.978441, -9.855615, 3.772213, 11.313177, 3.9716103, -14.144719, 0.014285767, 6.570724, 3.438286, -9.297457, -24.61491, -44.020687, -53.023544, -54.29259, -51.721676, -43.92574, -36.236057, -25.067955, -8.515062, 2.1587827, 2.171175, -7.3301344, -24.340292, -36.819004, -46.151527, -49.122375, -46.875614, -36.59842, -25.150541, -8.381048,  3.39345, 10.194889, 10.559715, -11.624424, -27.953388, -43.099766, -52.23424, -53.29581, -49.35047, -36.064533, -20.837297, -4.5963764, 3.8120506, 6.553082, -0.0438216, -16.449749, -33.432564, -50.47874, -54.630085, -53.260944, -40.72143, -25.989126, -12.234187, -5.503456,  -5.3449, -18.764069, -31.827797, -39.053196, -54.333138, -55.142765, -53.77716, -50.84636, -40.912357 };
+
+
+
 
     // Classes that inherit from AbstractDeviceListener can be used to receive events from Myo devices.
     // If you do not override an event, the default behavior is to do nothing.
@@ -96,13 +120,111 @@ public class HelloWorldActivity extends Activity {
             mTextView.setRotation(roll);
             mTextView.setRotationX(pitch);
             mTextView.setRotationY(yaw);
+
+            //Datapoint dp = new Datapoint(roll, pitch, yaw);
+            //data_list.add(dp);
+            ts_count++;
+
+            if(ts_count == 1){
+                ts_begin = timestamp;
+                Log.d("","TIMESTAMP INITIAL: " + ts_begin);
+            }
+
+
+
+            if(timestamp - ts_begin <= 10000){
+                if(timestamp - ts_prev > ts_interval){
+
+                    Log.d("","TIMESTAMP: " + timestamp);
+                    //Log.d("Data   ","ROLL: " + String.valueOf(roll));// + " PITCH: " + String.valueOf(pitch) + " YAW: " + String.valueOf(yaw));
+                    //Log.d("", String.valueOf(timestamp));
+                    //Log.d("", "ROLL: " + String.valueOf(roll) + "    PITCH: " + String.valueOf(pitch) + "    YAW: " + String.valueOf(yaw));
+                    //Log.d("Data   ","YAW: " + String.valueOf(roll));
+                    Datapoint dp = new Datapoint(timestamp,roll, pitch, yaw);
+                    //Log.d("", "ROLL: " + String.valueOf(roll) + "    PITCH: " + String.valueOf(pitch) + "    YAW: " + String.valueOf(yaw));
+                    data_list.add(dp);
+
+
+
+                    //if(pitch >= p_prev && p_prev >= p_prevprev){
+                        //keep going
+                    /*if (pitch < p_prev && p_prev >= p_prevprev) {
+                        peak_pitch.add(pitch);
+                        Log.d("adding peak", String.valueOf(pitch));
+                    }
+                    else if (pitch > p_prev && p_prev <= p_prevprev){
+                        dip_pitch.add(pitch);
+                        Log.d("adding dip", String.valueOf(pitch));
+                    }
+
+
+                    p_prevprev = p_prev;
+                    p_prev = pitch;
+                    */
+
+
+                    ts_prev = timestamp;
+
+                }
+
+            }else if(timestamp - ts_begin > 10000){
+
+                if(listdata){
+                    Log.d("SIZE of data list:   ", String.valueOf(data_list.size()));
+                    for (int i=0; i < data_list.size(); i++){
+
+                        Log.d("", "TIMESTAMP:  " + String.valueOf(data_list.get(i).getTimestamp()) + "ROLL: " + String.valueOf(data_list.get(i).getRoll()) + "    PITCH: " + String.valueOf(data_list.get(i).getPitch()) + "    YAW: " + String.valueOf(data_list.get(i).getYaw()));
+                    }
+
+                    for (int i=0; i < peak_pitch.size(); i++){
+                        Log.d("PEAK   ", String.valueOf(peak_pitch.get(i)));
+                    }
+
+                    for (int i=0; i < dip_pitch.size(); i++){
+                        Log.d("DIP   ", String.valueOf(dip_pitch.get(i)));
+                    }
+                    listdata = false;
+
+                    p_prevprev = (float) dlist[0];
+                    p_prev = (float) dlist[1];
+
+                    for(int i=2; i < dlist.length; i++){
+                        pitch = (float) dlist[i];
+
+                        if (pitch < p_prev && p_prev >= p_prevprev) {
+                            peak_pitch.add(pitch);
+                            Log.d("adding peak", String.valueOf(pitch));
+                        }
+                        else if (pitch > p_prev && p_prev <= p_prevprev){
+                            dip_pitch.add(pitch);
+                            Log.d("adding dip", String.valueOf(pitch));
+                        }
+
+                        p_prevprev = p_prev;
+                        p_prev = pitch;
+                    }
+
+
+
+
+
+                }
+
+            }
+            //Log.d("Data   ","ROLL: " + String.valueOf(roll));// + " PITCH: " + String.valueOf(pitch) + " YAW: " + String.valueOf(yaw));
+            //Log.d("", String.valueOf(timestamp));
+            //Log.d("", "ROLL: " + String.valueOf(roll) + "    PITCH: " + String.valueOf(pitch) + "    YAW: " + String.valueOf(yaw));
+            //Log.d("Data   ","YAW: " + String.valueOf(roll));
+
+
+
         }
 
         // onPose() is called whenever a Myo provides a new pose.
         @Override
         public void onPose(Myo myo, long timestamp, Pose pose) {
             // Handle the cases of the Pose enumeration, and change the text of the text view
-            // based on the pose we receive.
+            // based on the pose we rec qeive.
             switch (pose) {
                 case UNKNOWN:
                     mTextView.setText(getString(R.string.hello_world));
